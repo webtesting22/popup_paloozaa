464c37924c5fa46c0e9ee765c82b189f982b2fec
HJ03 <102145684+HJ03@users.noreply.github.com>
2024-03-13 15:34:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,3 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('.main-section'),
     smooth: true
 });
-const Locoscroll = new LocomotiveScroll({
-    el: document.querySelector("[data-scroll-container]"),
-    smooth: true,
-    smartphone: {
-          smooth: true
-      }
-    //reloadOnContextChange: true
-  });
