66292088c87bd99a782bb1fc17b25ce630a588bf
HJ03 <102145684+HJ03@users.noreply.github.com>
2024-03-14 15:48:57 +0530

    popup added on gallery images

--- script.js
+++ script.js
@@ -44,119 +44,28 @@ function scrollToSmoothly(to, duration) {
     
     animateScroll();
 }
+$(document).ready(function() {
 
-/*
-  * Copyright (c) 2023 Giovanni Antonio
-  * All rights reserved. This code is licensed under the MIT.
-  * Unauthorized use, modification, or distribution is strictly prohibited.
-*/
-
-// document.querySelectorAll(".picture").forEach((p, i) => {
-// 	let r = Math.random() * 255;
-// 	let g = Math.random() * 255;
-// 	let b = Math.random() * 255;
-// 	let color = `rgba(${r},${g},${b},1)`;
-// 	p.style.backgroundColor = color;
-// 	p.style.color = `${brightness(r, g, b) < 130 ? "#fff" : "#000"}`;
-// 	// p.textContent = p.style.backgroundColor;
-// });
-
-const root = document.documentElement;
-
-let close = document.querySelector("#close");
-let gallery = document.querySelector(".gallery");
-let current = 0;
-
-// window.addEventListener(
-// 	"resize",
-// 	function () {
-// 		setTimeout(function () {
-// 			// update();
-// 		}, 1000);
-// 	},
-// 	{ passive: true }
-// );
-
-function update() {
-	if (!current) return;
-	let a = current.getBoundingClientRect();
-	let b = document.querySelector(".picture-clone");
-	b.style.width = a.width + "px";
-	b.style.height = a.height + "px";
-	let c = b.getBoundingClientRect();
-
-	let p = dist(a, c);
-	let s = scale(a);
-	let t = `translate(${(p.x / window.innerWidth) * 100}vw,${
-		(p.y / window.innerHeight) * 100
-	}vh) scale(${s.z}) translateZ(0)`;
-console.table(s)
-	current.style.transform = t;
-}
-
-function reset() {
-	let t = `translate(0,0) scale(1,1)`;
-	current.style.transform = t;
-	gallery.classList.add("close");
-	setTimeout(() => {
-		current.classList.remove("active");
-		current = 0;
-		gallery.classList.remove("open");
-		gallery.classList.remove("close");
-		root.classList.remove("fs");
-	}, 2000);
-}
-
-close.addEventListener("click", reset);
-
-gallery.addEventListener("click", (e) => {
-	if (e.target === e.currentTarget) return;
-
-	if (e.target.classList.contains("picture")) {
-		// create a function that works when window resizes and update target position
-
-		if (!current) {
-			// first active
-			current = e.target;
-		} else {
-			// remove old and init new
-			current.classList.remove("active");
-			current = e.target;
-		}
-
-		// no scroll fullscreen
-		root.classList.add("fs");
-		// activate now
-		current.classList.add("active");
-
-		gallery.classList.toggle("open");
-
-		update();
-		e.preventDefault();
-		e.stopPropagation();
-	}
-});
-
-function scale(a) {
-	let v =
-		window.innerWidth >= window.innerHeight
-			? window.innerWidth / a.width
-			: window.innerHeight / a.height;
-	return {
-		x: window.innerWidth / a.width,
-		y: window.innerHeight / a.height,
-		z: v
-	};
-}
-
-function dist(a, b) {
-	return {
-		x: b.x - a.x,
-		y: b.y - a.y
-	};
-}
-
-function brightness(r, g, b) {
-	var light = Math.sqrt(0.241 * r * r + 0.691 * g * g + 0.068 * b * b);
-	return light;
-}
+	// required elements
+	var imgPopup = $('.img-popup');
+	var imgCont  = $('.container__img-holder');
+	var popupImage = $('.img-popup img');
+	var closeBtn = $('.close-btn');
+  
+	// handle events
+	imgCont.on('click', function() {
+	  var img_src = $(this).children('img').attr('src');
+	  imgPopup.children('img').attr('src', img_src);
+	  imgPopup.addClass('opened');
+	});
+  
+	$(imgPopup, closeBtn).on('click', function() {
+	  imgPopup.removeClass('opened');
+	  imgPopup.children('img').attr('src', '');
+	});
+  
+	popupImage.on('click', function(e) {
+	  e.stopPropagation();
+	});
+	
+  });
